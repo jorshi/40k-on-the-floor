e5bcf04cf8f96a3e243f5ec9743bb07aee77b359
jorshi <jordieshier@gmail.com>
2023-02-20 15:52:55 +0000

    normalizing samples

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ from tqdm import tqdm
 
 SAMPLE_RATE = 48000
 
+# Only analyze the first half second
+ANALYSIS_SAMPLES = SAMPLE_RATE // 2
+
 
 def get_wav_files(input_dir: str):
     """
@@ -30,15 +33,25 @@ def process_files(wav_files: List[str]):
     mel = torchaudio.transforms.MelSpectrogram(
         sample_rate=SAMPLE_RATE, n_fft=2048, hop_length=128, n_mels=128
     )
+    features = []
     for f in tqdm(wav_files):
         audio, sample_rate = torchaudio.load(f)
         # Resample if needed
         if sample_rate != SAMPLE_RATE:
             audio = torchaudio.transforms.Resample(sample_rate, SAMPLE_RATE)(audio)
 
+        # Trim the audio
+        audio = audio[:, :ANALYSIS_SAMPLES]
+
         # Get the mel spectrogram
         mel_spec = mel(audio)
         mel_stats = temporal_stats(mel_spec)
+        features.append(mel_stats)
+
+    # Stack all the features
+    features = torch.vstack(features)
+
+    return features
 
 
 def temporal_stats(x: torch.Tensor):
@@ -51,8 +64,24 @@ def temporal_stats(x: torch.Tensor):
     std_diff = x.diff(dim=2).std(dim=2)
 
     # Stack
-    stats = torch.hstack([mean, std, mean_diff, std_diff])
+    stats = torch.stack([mean, std, mean_diff, std_diff], dim=-1)
     return stats
+
+
+def normalize_features(features: torch.Tensor):
+    """
+    Normalize the features
+    """
+    assert features.ndim == 3, "Input tensor must be 3D: (batch, freq_bins, stats)"
+
+    for i in range(features.shape[-1]):
+        features[:, :, i] = (features[:, :, i] - features[:, :, i].min()) / (
+            features[:, :, i].max() - features[:, :, i].min()
+        )
+        assert torch.all(features[:, :, i] >= 0) and torch.all(features[:, :, i] <= 1)
+
+    assert torch.all(features >= 0) and torch.all(features <= 1)
+    return features
 
 
 def main(arguments):
@@ -69,7 +98,10 @@ def main(arguments):
     wav_files = get_wav_files(args.input_dir)
 
     # Process all the files
-    process_files(wav_files)
+    features = process_files(wav_files)
+
+    # Normalize the features
+    features = normalize_features(features)
 
 
 if __name__ == "__main__":
